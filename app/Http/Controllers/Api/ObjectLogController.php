23b5c45a2b5411bd8f1c1c469e111969a6f42185
Gumkle <dziedzicdev@gmail.com>
2018-08-30 22:53:06 +0200

    Added models and migrations to basic project structure

---
+++ app/Http/Controllers/Api/ObjectLogController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\ObjectLog;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ObjectLogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ObjectLog  $objectLog
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ObjectLog $objectLog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ObjectLog  $objectLog
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ObjectLog $objectLog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ObjectLog  $objectLog
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ObjectLog $objectLog)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Local/ObjectLogController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Local;
+
+use App\ObjectLog;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ObjectLogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ObjectLog  $objectLog
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ObjectLog $objectLog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ObjectLog  $objectLog
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ObjectLog $objectLog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ObjectLog  $objectLog
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ObjectLog $objectLog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ObjectLog  $objectLog
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ObjectLog $objectLog)
+    {
+        //
+    }
+}
